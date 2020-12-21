b37ae463bc07e231f2bc9913fd5e3c07f9927e93
System Administrator <siethdanial@gmail.com>
2020-12-21 18:24:52 +0500

    client added

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   MONGODB:
-    'mongodb+srv://danial1122:danial1122@cluster0.eawwm.mongodb.net/social?retryWrites=true&w=majority',
+    'mongodb://danial1122:danial1122@cluster0-shard-00-00.eawwm.mongodb.net:27017,cluster0-shard-00-01.eawwm.mongodb.net:27017,cluster0-shard-00-02.eawwm.mongodb.net:27017/social?ssl=true&replicaSet=atlas-11nj4r-shard-0&authSource=admin&retryWrites=true&w=majority',
   SECRET_KEY: 'some very secret key'
 }
